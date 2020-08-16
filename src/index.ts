8cc6a20ca77f4c38458a53f7c425427530a8c4d5
shin donggyu <donggyu9410@gmail.com>
2020-08-17 01:30:43 +0900

    something changed

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,41 @@ import mongoose from 'mongoose'
 import puppeteer from 'puppeteer'
 
 interface scrapingResult {
-  title:string 
-  datePosted:string 
-  neighborhood:string 
-  url:string 
-  jodDescription:string
-  compensation:string
+  title: string
+  datePosted: string
+  neighborhood: string
+  url: string
+  jodDescription: string
+  compensation: string
 }
 
 interface IListing extends mongoose.Document {
-  title:string 
-  datePosted:string 
-  neighborhood:string 
-  url:string 
-  jodDescription:string
-  compensation:string
+  title: string
+  datePosted: string
+  neighborhood: string
+  url: string
+  jodDescription: string
+  compensation: string
 }
 const ListingSchema = new mongoose.Schema({
-  title:String, 
-  datePosted:String ,
-  neighborhood:String ,
-  url:String ,
-  jodDescription:String,
-  compensation:String
-  
+  title: String,
+  datePosted: String,
+  neighborhood: String,
+  url: String,
+  jodDescription: String,
+  compensation: String
+
 })
 
 const ListingModel = mongoose.model<IListing>('Listing', ListingSchema)
 
 
 
-const scrapeListings = async (page:puppeteer.Page) => {
+const scrapeListings = async (page: puppeteer.Page) => {
   await page.goto("https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof")
   const html = await page.content()
   const $ = cheerio.load(html)
-  const scrapingResults = $(".result-info").map((index, element):scrapingResult => {
+  const scrapingResults = $(".result-info").map((index, element): scrapingResult => {
     const titleElement = $(element).find("a.result-title")
     const timeElement = $(element).find("time.result-date")
     const hoodElement = $(element).find("span.result-hood")
@@ -50,40 +50,40 @@ const scrapeListings = async (page:puppeteer.Page) => {
       url,
       datePosted,
       neighborhood,
-      jodDescription:'',
-      compensation:''
+      jodDescription: '',
+      compensation: ''
     }
-}).get()
-return scrapingResults
+  }).get()
+  return scrapingResults
 }
 
-const scrapeListingsWithDescription = async (page:puppeteer.Page, listings:scrapingResult[]) => {
-    for (let index = 0; index < listings.length; index++) {
-      const listing = listings[index];
-      await page.goto(listing.url)
-      const html = await page.content()
-      const $ = cheerio.load(html)
-      const description = $("#postingbody").text()
-      const compensation = $("body > section > section > section > div.mapAndAttrs > p > span:nth-child(1) > b").text()
-      listing.jodDescription = description
-      listing.compensation = compensation
-      const listingData = new ListingModel(listing)
-      await listingData.save()
-      await sleep(2000)
-    }
+const scrapeListingsWithDescription = async (page: puppeteer.Page, listings: scrapingResult[]) => {
+  for (let index = 0; index < listings.length; index++) {
+    const listing = listings[index];
+    await page.goto(listing.url)
+    const html = await page.content()
+    const $ = cheerio.load(html)
+    const description = $("#postingbody").text()
+    const compensation = $("body > section > section > section > div.mapAndAttrs > p > span:nth-child(1) > b").text()
+    listing.jodDescription = description
+    listing.compensation = compensation
+    const listingData = new ListingModel(listing)
+    await listingData.save()
+    await sleep(2000)
+  }
 }
 
-function sleep(ms:number) {
+function sleep(ms: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-}   
+}
 
 const main = async () => {
-  await mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-  const brower = await puppeteer.launch({headless:false})
-  const page = await brower.newPage()
-  const listings:scrapingResult[] = await scrapeListings(page)
+  await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  const listings: scrapingResult[] = await scrapeListings(page)
   scrapeListingsWithDescription(page, listings)
 
 }
